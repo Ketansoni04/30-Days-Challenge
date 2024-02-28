6ef245a4df2d3cc50f0f40244fb7010edf3ebfb7
ketan soni <ketan504.ks@gmail.com>
2024-02-28 14:39:39 -0800

    Day01/30

---
+++ codes/JavaScript/Day1/variables.js
@@ -0,0 +1,17 @@
+const accountId = 144335
+let accountEmail = "ketan384@email.com"
+var accountPassword = "ks2342"
+accountCity = "Dewas"
+
+// accountId = 1789999 not allowd
+
+accountEmail = "ketanks@email.com"
+accountPassword = 'jlklds'
+accountCity = 'indore'
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
+
+/*
+please do not use var
+because of issue in block scope and functonal scope 
+*/
