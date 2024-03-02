d95a94aeca6e96bf4c3283929e1f7cc870dc21e7
ketan soni <ketan504.ks@gmail.com>
2024-03-02 10:46:20 -0800

    Day2/30-Done

---
+++ codes/JavaScript/Day2/conversionOperation.js
@@ -0,0 +1,29 @@
+let score = 33
+// console.log(typeof score)
+// console.log(typeof(score));
+
+
+
+let accountID = "ketan"
+
+let valueInNumber  = Number(accountID);
+// console.log( typeof valueInNumber);
+// console.log( valueInNumber);
+
+// "33" -> 33
+// "33abc" -> NaN
+// true -> 1; false -> 0
+
+let isLoggedIn = "ketan";
+
+let booleanILoggedIn = Boolean(isLoggedIn)
+// console.log(typeof booleanILoggedIn);
+// console.log(booleanILoggedIn);
+
+
+let someNumber = 33;
+
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber);
+console.log(stringNumber);
+
